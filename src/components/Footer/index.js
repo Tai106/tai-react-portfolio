861e254fbf610574578d7f98d6e4695c59c0c154
Gravity <tevoned@gmail.com>
2022-05-29 01:00:07 -0400

    navigation bar

---
+++ src/components/Footer/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/Tai106"
+					target="_blank"
+					rel=""
+				>
+					<img
+						src={require("../../assets/logos/github-logo.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.linkedin.com/in/taivon-edwards-0823b5b0"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/linkedin-logo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href=""
+					target="_blank"
+					rel=""
+				>
+					<img
+						src={require("../../assets/logos/twitter-logo.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
